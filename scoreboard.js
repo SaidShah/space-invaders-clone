6aecd63bf437e1498b2930e170de47ab15a59b2d
said shah <saidmysr20@aol.com>
2018-12-02 17:28:55 -0500

    added background color and bug fix

--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,5 @@
 function ScoreBoard() {
- let timer = 25;
+ let timer = 4;
 
   this.addPoint = function() {
     const addPt = document.getElementById("total-points").innerText
@@ -16,12 +16,13 @@ function ScoreBoard() {
   this.timerCounter = function() {
     let timerShown = document.getElementsByClassName("countDown")
     if (frameCount % 60 === 0 && timer > 0) {
-      timer--;
-      timerShown[0].innerText = timer
+      //timer--;
+      timerShown[0].innerText = `${--timer}`
 
     }
     if (timer === 0) {
       timerShown[0].innerText = "Time Is UP"
+      
       timerShown[0].style.color = "red"
     }
 
